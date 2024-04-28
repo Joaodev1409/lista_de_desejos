8d74d90c7e6f4116775275a4cd588d40dc68a7cf
joao <guimaraesjoaovictor23@gmail.com>
2024-04-28 14:22:25 -0300

    att

--- neymar.c
+++ neymar.c
@@ -1,34 +1,102 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Jogo{
-    char nome_do_jogo[51];
-    int codigo;
-    int preco;
-}JOGO;
+typedef struct NO {
+    char dado[100];
+    struct NO* prox;
+} NO;
 
-typedef struct NO{
-    int dado;
-    struct NO* proximo;
-    struct NO* anterior;
-}NO;
+typedef struct Lista {
+    NO* inicio;
+    NO* fim;
+} Lista;
 
-void InserirElemento(struct JOGO *elemento){
-    NO *ptr= (NO*)malloc(sizeof(NO));
-    ptr->anterior=NULL;
-    ptr->proximo=NULL;
-    ptr->dado=elemento;
-    
+Lista L;
+
+void InserirNoInicio(char* elemento) {
+    NO *ptr = (NO*)malloc(sizeof(NO));
+    if(ptr == NULL){
+        printf("Erro de Alocação!!!.\n");
+        exit(0);
+    } else {
+        strcpy(ptr->dado, elemento);
+        ptr->prox = NULL;
+        
+        if(L.inicio == NULL){
+            L.fim = ptr;
+        } else {
+            ptr->prox = L.inicio;
+        }
+        L.inicio = ptr;
+    }
 }
 
+void InserirNoFim(char* elemento) {
+    NO *ptr = (NO*)malloc(sizeof(NO));
+    if(ptr == NULL){
+        printf("Erro de Alocação!!!.\n");
+        exit(0);
+    } else {
+        strcpy(ptr->dado, elemento);
+        ptr->prox = NULL;
 
-void removerelemento(){
-    int op;
-    printf("1 ---REMOVER NO INICIO\n");
-    printf("2 ---REMOVER NO FINAL\n");
-    printf("3 ---REMOVER ELEMENTO INDICADO\n");
-    printf("0 ---NÃO REMOVER\n");
-    scanf("%d",&op);
+        if(L.inicio == NULL){
+            L.inicio = ptr;
+        } else {
+            L.fim->prox = ptr;
+        }
+        L.fim = ptr;
+    }
+}
 
+void imprimir() {
+    NO* ptr = L.inicio;
+    printf("INICIO---> ");
+    while(ptr != NULL){
+        printf("%s ", ptr->dado);
+        ptr = ptr->prox;
+    }
+    printf(" <---FIM\n\n");
+}
+
+void menu() {
+    printf("1- Para inserir no inicio da Lista\n");
+    printf("2- Para inserir no fim da Lista\n");
+    printf("3- Para imprimir a Lista\n");
+    printf("0- Para encerrar\n");
+}
+
+int main() {
+    int opcao;
+    char dado[100];
+    L.inicio = NULL;//sempre o inicio e o fim tem q ta inicializado em NULL
+    L.fim = NULL;
+    
+    do {
+        menu();
+        scanf("%d", &opcao);
+        getchar(); // é pra limpar o buffer do teclado , tem muita utilidade n mas é legal pra ficar bunitin
+        switch (opcao) {
+            case 0:
+                break;
+            case 1: //inserir no inicio
+                printf("Informe o nome do Jogo: ");
+                fgets(dado, 100, stdin);
+                dado[strcspn(dado, "\n")] = 0; // Remove o '\n' do final da string
+                InserirNoInicio(dado);
+                break;
+            case 2: //inserir no fim
+                printf("Informe o nome do Jogo: ");
+                fgets(dado, 100, stdin);
+                dado[strcspn(dado, "\n")] = 0; // Remove o '\n' do final da string
+                InserirNoFim(dado);
+                break;
+            case 3: //imprimir
+                imprimir();
+                break;
+        }
+    } while(opcao != 0);
+    
+    return 0;
 }
