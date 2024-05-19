807f6efecbd479a9de160a33dbd437486ebacd33
art-83 <arthurfmedeiros77@gmail.com>
2024-05-19 14:58:08 -0300

    registro_encadeado_completo

--- cadatro_lista_encadeada.c
+++ cadatro_lista_encadeada.c
@@ -2,28 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct conta
-{
-    char *user;
+struct senhaNode {
     char *senha;
+    struct senhaNode *prox;
+};
+
+struct conta {
+    char *user;
     struct conta *prox;
+    struct senhaNode *senhas;
 };
 
 typedef struct conta Conta;
+typedef struct senhaNode SenhaNode;
 
-Conta* criarConta()
-{
+Conta* criarConta() {
     Conta* novaConta = (Conta*)malloc(sizeof(Conta));
     novaConta->user = NULL;
-    novaConta->senha = NULL;
     novaConta->prox = NULL;
+    novaConta->senhas = NULL;
     return novaConta;
 }
 
-void Cadastro(Conta *inicio)
-{
-    Conta *atual = inicio;
+SenhaNode* criarSenhaNode() {
+    SenhaNode* novaSenha = (SenhaNode*)malloc(sizeof(SenhaNode));
+    novaSenha->senha = NULL;
+    novaSenha->prox = NULL;
+    return novaSenha;
+}
 
+void Cadastro(Conta *conta) {
     char usuario[20], senha1[20], senha2[20];
 
     printf("======REGISTRO======\n\nRegistre seu nome de usuario:\n");
@@ -33,55 +41,59 @@ void Cadastro(Conta *inicio)
     printf("Confirme sua senha:\n");
     scanf("%s", senha2);
 
-    while (1)
-    {
-        if (strcmp(senha1, senha2) != 0)
-        {
+    while (1) {
+        if (strcmp(senha1, senha2) != 0) {
             printf("As senhas nao correspondem. Tente de novo:\n");
             scanf("%s", senha1);
             scanf("%s", senha2);
-        }
-        else
-        {
+        } else {
             printf("Registro efetuado!\n");
             break;
         }
     }
 
-    atual->user = malloc(strlen(usuario) + 1);
-    strcpy(atual->user, usuario);
+    conta->user = malloc(strlen(usuario) + 1);
+    strcpy(conta->user, usuario);
 
-    atual->senha = malloc(strlen(senha1) + 1);
-    strcpy(atual->senha, senha1);
+    SenhaNode *novaSenha = criarSenhaNode();
+    novaSenha->senha = malloc(strlen(senha1) + 1);
+    strcpy(novaSenha->senha, senha1);
+
+    if (conta->senhas == NULL) {
+        conta->senhas = novaSenha;
+    } else {
+        SenhaNode *atualSenha = conta->senhas;
+        while (atualSenha->prox != NULL) {
+            atualSenha = atualSenha->prox;
+        }
+        atualSenha->prox = novaSenha;
+    }
 }
 
-void imprimirContas(Conta *inicio)
-{
+void imprimirContas(Conta *inicio) {
     Conta *atual = inicio;
-
-    while (atual != NULL)
-    {
-        printf("Usuário: %s\nSenha: %s\n\n", atual->user, atual->senha);
+    while (atual != NULL) {
+        printf("Usuário: %s\n", atual->user);
+        SenhaNode *senhaAtual = atual->senhas;
+        while (senhaAtual != NULL) {
+            printf("Senha: %s\n", senhaAtual->senha);
+            senhaAtual = senhaAtual->prox;
+        }
+        printf("\n");
         atual = atual->prox;
     }
 }
 
-int main()
-{
+int main() {
     Conta *inicio = NULL;
     Conta *atual;
-
     char continuar;
 
-    do
-    {
-        if (inicio == NULL)
-        {
+    do {
+        if (inicio == NULL) {
             inicio = criarConta();
             atual = inicio;
-        }
-        else
-        {
+        } else {
             atual->prox = criarConta();
             atual = atual->prox;
         }
@@ -94,5 +106,22 @@ int main()
 
     printf("\n======= REGISTROS =======\n\n");
     imprimirContas(inicio);
+
+    Conta *contaAtual = inicio;
+    while (contaAtual != NULL) {
+        Conta *contaParaLiberar = contaAtual;
+        contaAtual = contaAtual->prox;
+
+        SenhaNode *senhaAtual = contaParaLiberar->senhas;
+        while (senhaAtual != NULL) {
+            SenhaNode *senhaParaLiberar = senhaAtual;
+            senhaAtual = senhaAtual->prox;
+            free(senhaParaLiberar->senha);
+            free(senhaParaLiberar);
+        }
+        free(contaParaLiberar->user);
+        free(contaParaLiberar);
+    }
+
     return 0;
 }
