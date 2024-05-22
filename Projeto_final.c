27dcd55f8baed367c4aed60fbf8911354a12b86f
joao <guimaraesjoaovictor23@gmail.com>
2024-05-22 19:50:54 -0300

    att

--- Projeto_final.c
+++ Projeto_final.c
@@ -3,11 +3,23 @@
 #include "cadatro_lista_encadeada.c"
 #include "listagem_dos_itens.c"
 
-void banco_txt(Conta *conta){
+void criar_banco_txt(Conta *conta,NO *dado){
     char filename[60];
     sprintf(filename,"%s_lista de desejos.txt",conta->user);
     FILE* file=fopen(filename,"w");
-    NO* lista=conta->inicio;
+    NO* lista=dado->dado;
+    while(lista!=NULL){
+        fprintf(file,"%s\n",lista->dado);
+        lista= lista->prox;
+    }
+    fclose(file);
+}
+
+void adicionar_ao_banco_de_dados(Conta *conta,NO *dado){
+    char filename[60];
+    sprintf(filename,"%s_lista de desejos.txt",conta->user);
+    FILE* file=fopen(filename,"a");
+    NO* lista=dado->dado;
     while(lista!=NULL){
         fprintf(file,"%s\n",lista->dado);
         lista= lista->prox;
@@ -19,10 +31,15 @@ int main(){
    /*Conta *inicio = NULL;
     Conta *atual;
     char continuar;
+    int opcao;
+    char dado[100];
+    L.inicio = NULL;//sempre o inicio e o fim tem q ta inicializado em NULL
+    L.fim = NULL;
 
     do {
         if (inicio == NULL) {
             inicio = criarConta();
+            banco_txt(inicio,dado);
             atual = inicio;
         } else {
             atual->prox = criarConta();
@@ -53,11 +70,7 @@ int main(){
         free(contaParaLiberar->user);
         free(contaParaLiberar);
     }
-   */
-    /*int opcao;
-    char dado[100];
-    L.inicio = NULL;//sempre o inicio e o fim tem q ta inicializado em NULL
-    L.fim = NULL;
+    
     
     do {
         menu();
@@ -71,12 +84,14 @@ int main(){
                 fgets(dado, 100, stdin);
                 dado[strcspn(dado, "\n")] = 0; // Remove o '\n' do final da string
                 InserirNoInicio(dado);
+                adicionar_ao_banco_de_dados(inicio,dado);
                 break;
             case 2: //inserir no fim
                 printf("Informe o nome do Jogo: ");
                 fgets(dado, 100, stdin);
                 dado[strcspn(dado, "\n")] = 0; // Remove o '\n' do final da string
                 InserirNoFim(dado);
+                adicionar_ao_banco_de_dados(inicio,dado);
                 break;
             case 3:
                 remover2(&L);
