4e77b78e0022804f51fa89745583703697110418
art-83 <arthurfmedeiros77@gmail.com>
2024-05-28 19:15:25 -0300

    projeto_C_fase_final

--- lista_de_desejos/Projeto_final.c
+++ lista_de_desejos/Projeto_final.c
@@ -2,107 +2,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "cadatro_lista_encadeada.c"
 #include "listagem_dos_itens.c"
-
-// Função para criar banco de dados em arquivo texto
-void criar_banco_txt(Conta *conta, const char *dado) {
-    if (conta == NULL) {
-        printf("Erro: conta é NULL\n");
-        return;
-    }
-    if (conta->user == NULL) {
-        printf("Erro: conta->user é NULL\n");
-        return;
-    }
-
-    char filename[60];
-    sprintf(filename, "%s_lista_de_desejos.txt", conta->user);
-    FILE *file = fopen(filename, "a");
-    if (file == NULL) {
-        printf("Erro ao criar banco de dados do usuário\n");
-        return;
-    }
-    fprintf(file, "%s\n", dado);
-    fclose(file);
-}
 
 int main() {
     Conta *inicio = NULL;
     Conta *atual;
     char continuar;
-    int opcao;
-    char dado[100];
+    char usuarioLogado[20] = {0};
 
-    // Inicializando lista
-    L.inicio = NULL;
-    L.fim = NULL;
-
-    // Cadastro inicial de contas
     do {
         if (inicio == NULL) {
             inicio = criarConta();
-            Cadastro(inicio);  // Inicializa os dados da conta
             atual = inicio;
         } else {
             atual->prox = criarConta();
             atual = atual->prox;
-            Cadastro(atual);  // Inicializa os dados da nova conta
         }
 
-        printf("Deseja adicionar outra conta? (s/n): ");
+        cadastrarConta(atual);
+
+        printf("Deseja adicionar outro registro? (s/n): ");
         scanf(" %c", &continuar);
     } while (continuar == 's');
 
-    // Exibição das contas cadastradas
-    printf("\n======= REGISTROS =======\n\n");
+    printf("\n============= REGISTROS =============\n\n");
     imprimirContas(inicio);
 
-    // Menu de opções
+    int logado = 0;
+    int opcao;
+
     do {
-        menu();
-        scanf("%d", &opcao);
-        getchar();  // Limpa o buffer do teclado
-        switch (opcao) {
-            case 0:
-                break;
-            case 1:  // Inserir no início
-                printf("Informe o nome do Jogo: ");
-                fgets(dado, 100, stdin);
-                dado[strcspn(dado, "\n")] = 0;  // Remove o '\n' do final da string
-                InserirNoInicio(dado);
-                criar_banco_txt(inicio, dado);
-                break;
-            case 2:  // Inserir no fim
-                printf("Informe o nome do Jogo: ");
-                fgets(dado, 100, stdin);
-                dado[strcspn(dado, "\n")] = 0;  // Remove o '\n' do final da string
-                InserirNoFim(dado);
-                criar_banco_txt(inicio, dado);
-                break;
-            case 3:  // Remover item da lista
-                remover2(&L);
-                break;
-            case 4:  // Imprimir lista
-                imprimir();
-                break;
+        if (!logado) {
+            char usuario[20], senha[20];
+            printf("============= LOGIN =============\n\nDigite seu nome de usuario:\n");
+            scanf("%s", usuario);
+            printf("Digite sua senha:\n");
+            scanf("%s", senha);
+
+            if (login(inicio, usuario, senha, usuarioLogado)) {
+                printf("Login bem-sucedido!\n");
+                logado = 1;
+
+            } else {
+                printf("Falha no login! Usuario ou senha incorretos.\n");
+                continue;
+            }
         }
+
+        do {
+            limparTerminal();
+            imprimirLista();
+            menu();
+            scanf("%d", &opcao);
+            char jogo[100];
+
+            switch (opcao) {
+                case 1:
+                    limparTerminal();
+                    printf("Informe o nome do jogo para inserir no inicio: ");
+                    scanf("%s", jogo);
+                    inserirNoInicio(jogo);
+                    salvarEmArquivo(usuarioLogado);
+                    break;
+                case 2:
+                    limparTerminal();
+                    printf("Informe o nome do jogo para inserir no fim: ");
+                    scanf("%s", jogo);
+                    inserirNoFim(jogo);
+                    salvarEmArquivo(usuarioLogado);
+                    break;
+                case 3:
+                    limparTerminal();
+                    removerItem(&L);
+                    salvarEmArquivo(usuarioLogado);
+                    break;
+                case 4:
+                    limparTerminal();
+                    logado = 0;
+                    break;
+                case 0:
+                    limparTerminal();
+                    break;
+                default:
+                    limparTerminal();
+                    printf("Opção inválida!\n");
+                    break;
+            }
+        } while (opcao != 0 && logado);
+
+        NO *noAtual = L.inicio;
+        while (noAtual != NULL) {
+            NO *noParaLiberar = noAtual;
+            noAtual = noAtual->prox;
+            free(noParaLiberar);
+        }
+        L.inicio = L.fim = NULL;
+
     } while (opcao != 0);
 
-    // Liberação de memória
     Conta *contaAtual = inicio;
     while (contaAtual != NULL) {
         Conta *contaParaLiberar = contaAtual;
         contaAtual = contaAtual->prox;
 
-        SenhaNode *senhaAtual = contaParaLiberar->senhas;
+        SenhaNo *senhaAtual = contaParaLiberar->senhas;
         while (senhaAtual != NULL) {
-            SenhaNode *senhaParaLiberar = senhaAtual;
+            SenhaNo *senhaParaLiberar = senhaAtual;
             senhaAtual = senhaAtual->prox;
             free(senhaParaLiberar->senha);
             free(senhaParaLiberar);
         }
-        free(contaParaLiberar->user);
+        free(contaParaLiberar->usuario);
         free(contaParaLiberar);
     }
 
--- lista_de_desejos/listagem_dos_itens.c
+++ lista_de_desejos/listagem_dos_itens.c
@@ -2,40 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Definição das estruturas para lista de contas e senhas
-typedef struct senhaNode {
+typedef struct senhaNo {
     char *senha;
-    struct senhaNode *prox;
-} SenhaNode;
+    struct senhaNo *prox;
+} SenhaNo;
 
 typedef struct conta {
-    char *user;
+    char *usuario;
     struct conta *prox;
-    struct senhaNode *senhas;
+    struct senhaNo *senhas;
 } Conta;
 
-// Função para criar uma nova conta
 Conta* criarConta() {
     Conta* novaConta = (Conta*)malloc(sizeof(Conta));
-    novaConta->user = NULL;
+    novaConta->usuario = NULL;
     novaConta->prox = NULL;
     novaConta->senhas = NULL;
     return novaConta;
 }
 
-// Função para criar um novo nó de senha
-SenhaNode* criarSenhaNode() {
-    SenhaNode* novaSenha = (SenhaNode*)malloc(sizeof(SenhaNode));
+SenhaNo* criarSenhaNo() {
+    SenhaNo* novaSenha = (SenhaNo*)malloc(sizeof(SenhaNo));
     novaSenha->senha = NULL;
     novaSenha->prox = NULL;
     return novaSenha;
 }
 
-// Função para cadastrar uma nova conta
-void Cadastro(Conta *conta) {
+void limparTerminal() {
+    system("cls");
+}
+
+void cadastrarConta(Conta *conta) {
     char usuario[20], senha1[20], senha2[20];
 
-    printf("======REGISTRO======\n\nRegistre seu nome de usuario:\n");
+    printf("============= REGISTRO =============\n\nRegistre seu nome de usuario:\n");
     scanf("%s", usuario);
     printf("Registre sua senha:\n");
     scanf("%s", senha1);
@@ -44,7 +44,7 @@ void Cadastro(Conta *conta) {
 
     while (1) {
         if (strcmp(senha1, senha2) != 0) {
-            printf("As senhas nao correspondem. Tente de novo:\n");
+            printf("As senhas nao correspondem. Tente de novo:\n\n");
             printf("Registre sua senha:\n");
             scanf("%s", senha1);
             printf("Confirme sua senha:\n");
@@ -55,14 +55,14 @@ void Cadastro(Conta *conta) {
         }
     }
 
-    conta->user = malloc(strlen(usuario) + 1);
-    if (conta->user == NULL) {
+    conta->usuario = malloc(strlen(usuario) + 1);
+    if (conta->usuario == NULL) {
         printf("Erro ao alocar memória para o usuário\n");
         exit(1);
     }
-    strcpy(conta->user, usuario);
+    strcpy(conta->usuario, usuario);
 
-    SenhaNode *novaSenha = criarSenhaNode();
+    SenhaNo *novaSenha = criarSenhaNo();
     novaSenha->senha = malloc(strlen(senha1) + 1);
     if (novaSenha->senha == NULL) {
         printf("Erro ao alocar memória para a senha\n");
@@ -73,20 +73,21 @@ void Cadastro(Conta *conta) {
     if (conta->senhas == NULL) {
         conta->senhas = novaSenha;
     } else {
-        SenhaNode *atualSenha = conta->senhas;
+        SenhaNo *atualSenha = conta->senhas;
         while (atualSenha->prox != NULL) {
             atualSenha = atualSenha->prox;
         }
         atualSenha->prox = novaSenha;
     }
+
+    limparTerminal();  
 }
 
-// Função para imprimir todas as contas e senhas
 void imprimirContas(Conta *inicio) {
     Conta *atual = inicio;
     while (atual != NULL) {
-        printf("Usuário: %s\n", atual->user);
-        SenhaNode *senhaAtual = atual->senhas;
+        printf("Usuario: %s\n", atual->usuario);
+        SenhaNo *senhaAtual = atual->senhas;
         while (senhaAtual != NULL) {
             printf("Senha: %s\n", senhaAtual->senha);
             senhaAtual = senhaAtual->prox;
@@ -96,43 +97,40 @@ void imprimirContas(Conta *inicio) {
     }
 }
 
-// Função de login
-int Login(Conta *inicio, const char *usuario, const char *senha, char *usuarioLogado) {
+int login(Conta *inicio, const char *usuario, const char *senha, char *usuarioLogado) {
     Conta *atual = inicio;
     while (atual != NULL) {
-        if (strcmp(atual->user, usuario) == 0) {
-            SenhaNode *senhaAtual = atual->senhas;
+        if (strcmp(atual->usuario, usuario) == 0) {
+            SenhaNo *senhaAtual = atual->senhas;
             while (senhaAtual != NULL) {
                 if (strcmp(senhaAtual->senha, senha) == 0) {
-                    strcpy(usuarioLogado, usuario); // Armazena o nome do usuário logado
-                    return 1; // Login bem-sucedido
+                    strcpy(usuarioLogado, usuario);
+                    return 1;
                 }
                 senhaAtual = senhaAtual->prox;
             }
         }
         atual = atual->prox;
     }
-    return 0; // Falha no login
+    return 0;
 }
 
-// Definição das estruturas para lista de jogos
 typedef struct NO {
     char dado[100];
     struct NO* prox;
 } NO;
 
-typedef struct Lista {
+typedef struct lista {
     NO* inicio;
     NO* fim;
 } Lista;
 
 Lista L = {NULL, NULL};
 
-// Função para inserir um elemento no início da lista
-void InserirNoInicio(char* elemento) {
+void inserirNoInicio(char* elemento) {
     NO *ptr = (NO*)malloc(sizeof(NO));
     if(ptr == NULL){
-        printf("Erro de Alocação!!!\n");
+        printf("erro de alocacao\n");
         exit(0);
     } else {
         strcpy(ptr->dado, elemento);
@@ -147,8 +145,7 @@ void InserirNoInicio(char* elemento) {
     }
 }
 
-// Função para inserir um elemento no fim da lista
-void InserirNoFim(char* elemento) {
+void inserirNoFim(char* elemento) {
     NO *ptr = (NO*)malloc(sizeof(NO));
     if(ptr == NULL){
         printf("Erro de Alocação!!!\n");
@@ -166,19 +163,16 @@ void InserirNoFim(char* elemento) {
     }
 }
 
-// Função para imprimir todos os elementos da lista
-void imprimir() {
+void imprimirLista() {
     NO* ptr = L.inicio;
-    printf("INICIO--->\n");
+    printf("Sua Wishlist: \n\n");
     while(ptr != NULL){
         printf("%s\n", ptr->dado);
         ptr = ptr->prox;
     }
-    printf("<---FIM\n\n");
 }
 
-// Função para remover um elemento da lista
-void remover2(Lista *l){
+void removerItem(Lista *l){
     NO *aux, *prev = NULL;
     aux = l->inicio;
     if(aux == NULL){
@@ -203,20 +197,18 @@ void remover2(Lista *l){
     }
 }
 
-// Função para exibir o menu
 void menu() {
+    printf("\n\n");
     printf("1- Para inserir no inicio da Lista\n");
     printf("2- Para inserir no fim da Lista\n");
     printf("3- Para remover item da Lista\n");
-    printf("4- Para imprimir a Lista\n");
-    printf("5- Para sair da conta\n");
+    printf("4- Para sair da conta\n");
     printf("0- Para encerrar\n");
 }
 
-// Função para salvar o usuário logado e a lista de jogos em um arquivo txt
-void salvarEmArquivo(const char* usuario, int session) {
+void salvarEmArquivo(const char* usuario) {
     char filename[50];
-    sprintf(filename, "dados_%s_%d.txt", usuario, session);
+    sprintf(filename, "dados_%s.txt", usuario);
     FILE *arquivo = fopen(filename, "w");
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo!\n");
@@ -234,15 +226,13 @@ void salvarEmArquivo(const char* usuario, int session) {
 
     fclose(arquivo);
 }
-
+/*
 int main() {
     Conta *inicio = NULL;
     Conta *atual;
     char continuar;
-    char usuarioLogado[20] = {0}; // Buffer para armazenar o nome do usuário logado
-    int session = 0;
+    char usuarioLogado[20] = {0};
 
-    // Criação de contas e login
     do {
         if (inicio == NULL) {
             inicio = criarConta();
@@ -252,75 +242,77 @@ int main() {
             atual = atual->prox;
         }
 
-        Cadastro(atual);
+        cadastrarConta(atual);
 
         printf("Deseja adicionar outro registro? (s/n): ");
         scanf(" %c", &continuar);
     } while (continuar == 's');
 
-    printf("\n======= REGISTROS =======\n\n");
+    printf("\n============= REGISTROS =============\n\n");
     imprimirContas(inicio);
 
-    int loggedIn = 0;
+    int logado = 0;
     int opcao;
 
     do {
-        if (!loggedIn) {
-            // Tentar login
+        if (!logado) {
             char usuario[20], senha[20];
-            printf("======LOGIN======\n\nDigite seu nome de usuario:\n");
+            printf("============= LOGIN =============\n\nDigite seu nome de usuario:\n");
             scanf("%s", usuario);
             printf("Digite sua senha:\n");
             scanf("%s", senha);
 
-            if (Login(inicio, usuario, senha, usuarioLogado)) {
+            if (login(inicio, usuario, senha, usuarioLogado)) {
                 printf("Login bem-sucedido!\n");
-                loggedIn = 1;
-                session++;
+                logado = 1;
+
             } else {
                 printf("Falha no login! Usuario ou senha incorretos.\n");
                 continue;
             }
         }
 
-        // Operações na lista de jogos
         do {
+            limparTerminal();
+            imprimirLista();
             menu();
             scanf("%d", &opcao);
             char jogo[100];
 
             switch (opcao) {
                 case 1:
+                    limparTerminal();
                     printf("Informe o nome do jogo para inserir no inicio: ");
                     scanf("%s", jogo);
-                    InserirNoInicio(jogo);
-                    salvarEmArquivo(usuarioLogado, session);
+                    inserirNoInicio(jogo);
+                    salvarEmArquivo(usuarioLogado);
                     break;
                 case 2:
+                    limparTerminal();
                     printf("Informe o nome do jogo para inserir no fim: ");
                     scanf("%s", jogo);
-                    InserirNoFim(jogo);
-                    salvarEmArquivo(usuarioLogado, session);
+                    inserirNoFim(jogo);
+                    salvarEmArquivo(usuarioLogado);
                     break;
                 case 3:
-                    remover2(&L);
-                    salvarEmArquivo(usuarioLogado, session);
+                    limparTerminal();
+                    removerItem(&L);
+                    salvarEmArquivo(usuarioLogado);
                     break;
                 case 4:
-                    imprimir();
-                    break;
-                case 5:
-                    loggedIn = 0; // Sair da conta atual
+                    limparTerminal();
+                    logado = 0;
                     break;
                 case 0:
+                    limparTerminal();
                     break;
                 default:
+                    limparTerminal();
                     printf("Opção inválida!\n");
                     break;
             }
-        } while (opcao != 0 && loggedIn);
+        } while (opcao != 0 && logado);
 
-        // Limpar a lista de jogos ao sair da conta
         NO *noAtual = L.inicio;
         while (noAtual != NULL) {
             NO *noParaLiberar = noAtual;
@@ -331,22 +323,22 @@ int main() {
 
     } while (opcao != 0);
 
-    // Liberação da memória alocada
     Conta *contaAtual = inicio;
     while (contaAtual != NULL) {
         Conta *contaParaLiberar = contaAtual;
         contaAtual = contaAtual->prox;
 
-        SenhaNode *senhaAtual = contaParaLiberar->senhas;
+        SenhaNo *senhaAtual = contaParaLiberar->senhas;
         while (senhaAtual != NULL) {
-            SenhaNode *senhaParaLiberar = senhaAtual;
+            SenhaNo *senhaParaLiberar = senhaAtual;
             senhaAtual = senhaAtual->prox;
             free(senhaParaLiberar->senha);
             free(senhaParaLiberar);
         }
-        free(contaParaLiberar->user);
+        free(contaParaLiberar->usuario);
         free(contaParaLiberar);
     }
 
     return 0;
 }
+*/
